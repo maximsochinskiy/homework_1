ca5b3d7e476701b8049540f611ee55011f7944e2
Maxim Sochinskiy <maxim.sochinskiy@gmail.com>
2018-08-26 15:26:07 +0300

    Задание 13.

--- homework_3(13).py
+++
@@ -1,9 +0,0 @@
-import math
-def cone_square_and_volume(radius, height):
-     square = math.pi * radius * math.sqrt(radius**2 + height**2)
-     volume = (height / 3)* math.pi * radius**2
-     return square, volume
-
-result_square, result_volume = cone_square_and_volume(5, 4)
-
-print('Results, are: %.2f, %.2f' % (result_square, result_volume))
