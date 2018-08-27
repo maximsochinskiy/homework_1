045875447ae2882764ad026a39beddb4d0c8ac03
Maxim Sochinskiy <maxim.sochinskiy@gmail.com>
2018-08-27 21:34:58 +0300

    Задание 13.

--- homework_2(10).py
+++ homework_2(10).py
@@ -1,3 +1,4 @@
+import datetime
 writer_name = input('ФИО писателя, дата рождения и дата смерти в формате: Имя Фамилия*гггг-мм-дд*гггг-мм-дд*:')
 
 inital_data = writer_name.split('*')
@@ -9,10 +10,16 @@ death_date = inital_data[2]
 birth_date_1 = birth_date.split('-')
 death_date_1 = death_date.split('-')
 
+"""""
 birth = int(birth_date_1[0])
 death = int(death_date_1[0])
+"""
 
-age = death - birth
+birth = datetime.date(int(birth_date_1[0]), int(birth_date_1[1]), int(birth_date_1[2]))
+death = datetime.date(int(death_date_1[0]), int(death_date_1[1]), int(death_date_1[2]))
 
-print(name, age)
+age_days = death - birth
+age_years = age_days/365
+
+print(name, age_years)
 
--- homework_3(12).py
+++ homework_3(12).py
@@ -1,10 +1,27 @@
+# по строкам
 def sum_of_digits (number):
-    one = int(number [0])
-    two = int(number[1])
-    three = int(number [2])
-    summa = one + two + three
+     one = int (number [0])
+     two = int (number [1])
+     three = int (number [2])
+     summa = one + two + three
+     return summa
+
+ number = input('Введите 3-х значное число:')
+
+ print('Result equals:', sum_of_digits(number))
+
+# без использования строк
+number1 = int(input('Введите 3-х значное число:'))
+def sum_of_digits (number1):
+    a = number1 // 100
+    b = (number1 // 10) % 10
+    c = number1 % 10
+    summa = a + b + c
     return summa
 
-number = input('Введите 3-х значное число:')
 
-print('Result equials:', sum_of_digits(number))
+result1 = sum_of_digits(number1)
+
+print('Result equals:', result1)
+
+
